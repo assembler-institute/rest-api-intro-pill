406b33ec797eca6417c578724a8ea7827774b9fa
Jcarri14 <j.carrion@students.salle.url.edu>
2021-06-30 11:08:20 +0200

    feat: add base person controller

---
+++ src/controllers/person-controller.js
@@ -0,0 +1,14 @@
+async function addPerson(req, res, next) {}
+
+async function updatePerson(req, res, next) {}
+
+async function fetchPersons(req, res, next) {}
+
+async function fetchPerson(req, res, next) {}
+
+module.exports = {
+  addPerson: addPerson,
+  updatePerson: updatePerson,
+  fetchPersons: fetchPersons,
+  fetchPerson: fetchPerson,
+};

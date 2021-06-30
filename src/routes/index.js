928ea5570108f3eedf9596904b88e7d04528e536
Jcarri14 <j.carrion@students.salle.url.edu>
2021-06-30 11:10:29 +0200

    feat: add serverjs & indexjs

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const personRouter = require("./person-routes");
+const movieRouter = require("./movie-routes");
+const movieGenreRouter = require("./movie-genres-routes");
+
+module.exports = {
+  personRouter,
+  movieRouter,
+  movieGenreRouter,
+};

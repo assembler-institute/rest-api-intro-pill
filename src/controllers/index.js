f58ed8d370a4c90487d3e2627876ef28b8e86023
Jcarri14 <j.carrion@students.salle.url.edu>
2021-06-30 11:08:59 +0200

    feat: add seed-data

---
+++ src/controllers/index.js
@@ -0,0 +1,9 @@
+const movieGenreController = require("./movie-genre-controller");
+const movieController = require("./movie-controller");
+const personController = require("./person-controller");
+
+module.exports = {
+  movieGenreController: movieGenreController,
+  movieController: movieController,
+  personController: personController,
+};

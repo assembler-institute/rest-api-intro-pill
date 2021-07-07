0c1530cd2f09a716957b0093a5c766a342c2979c
Jcarri14 <j.carrion@students.salle.url.edu>
2021-07-07 12:26:21 +0200

    feat: add account + user routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,9 +1,15 @@
 const movieGenreController = require("./movie-genre-controller");
+const movieCreditsController = require("./movie-credits-controller");
 const movieController = require("./movie-controller");
 const personController = require("./person-controller");
+const userController = require("./user-controller");
+const authController = require("./auth-controller");
 
 module.exports = {
+  authController: authController,
+  userController: userController,
   movieGenreController: movieGenreController,
+  movieCreditsController: movieCreditsController,
   movieController: movieController,
   personController: personController,
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,12 @@
-const personRouter = require("./person-routes");
-const movieRouter = require("./movie-routes");
-const movieGenreRouter = require("./movie-genres-routes");
+const { userRouter } = require("./user-routes");
+const { personRouter } = require("./person-routes");
+const { movieRouter } = require("./movie-routes");
+const { movieGenreRouter } = require("./movie-genres-routes");
+const { accountRouter } = require("./account-routes");
 
 module.exports = {
+  userRouter,
+  accountRouter,
   personRouter,
   movieRouter,
   movieGenreRouter,
